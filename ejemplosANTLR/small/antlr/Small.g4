17d4c38c1b366070554161f06637b3780cf60655
María Clarita Osorio <makisove@gmail.com>
2024-05-30 19:42:26 -0600

    First Push

--- ejemplosANTLR/small/antlr/Small.g4
+++ ejemplosANTLR/small/antlr/Small.g4
@@ -1,60 +1,80 @@
-grammar Small;           
+grammar Small;
 
 program
-    :   (decl|statement)*
+    :   (decl|function_decl|statement)*
     ;
 
 decl
-    :   INTEGER ident_list ';'      #intDecl
-    |   BOOL ident_list ';'         #boolDecl
-    |   ID 'as' type '(' ident_list ')' '{' statement* '}' #function_call
+    :   type ident_list ';'      #varDecl
     ;
 
 type
-    :
-       INTEGER
-       | BOOL
-       | STRING
-      ;
+    :   'int'
+    |   'float'
+    |   'string'
+    |   'bool'
+    ;
+
+function_decl
+    :   type ID '(' params? ')' '{' statement* '}'
+    ;
+
+params
+    :   param (',' param)*
+    ;
+
+param
+    :   type ID
+    ;
 
 ident_list
-    :   ident ( ',' ident )* 
+    :   ident (',' ident)*
     ;
 
 ident
-    : ID ( '=' expr ) ?
+    : ID ( '=' expr )?
     ;
 
 statement
-    :  expr '=' expr ';'                   #assign
-    |  PRINT '(' expr ')' ';'              #print
-    |  IF '(' expr ')' '{' statement* '}'  #if
-    | 'return' expr #return
+    :  type ident_list ';'                #varDeclStmt
+    |  expr '=' expr ';'                  #assign
+    |  type ID '=' expr ';'               #varInitStmt
+    |  'print' '(' expr ')' ';'           #print
+    |  'if' '(' expr ')' '{' statement* '}' ('else' '{' statement* '}')? #if
+    |  'while' '(' expr ')' '{' statement* '}' #while
+    |  'return' expr ';'                  #return
     ;
 
 expr
     :   '!' expr        #not
     |   expr '==' expr  #equal
+    |   expr '!=' expr  #notEqual
+    |   expr '>' expr   #greater
+    |   expr '<' expr   #less
+    |   expr '>=' expr  #greaterEqual
+    |   expr '<=' expr  #lessEqual
     |   expr '&&' expr  #and
+    |   expr '||' expr  #or
     |   expr '*' expr   #mult
     |   expr '/' expr   #div
     |   expr '+' expr   #add
     |   expr '-' expr   #sub
+    |   expr '^' expr   #pow
+    |   'sqrt' '(' expr ')'  #sqrt
     |   INT             #int
     |   STRING          #string
+    |   FLOAT           #float
     |   'true'          #true
     |   'false'         #false
     |   ID              #id
+    |   ID '(' (expr (',' expr)*)? ')'  #functionCall
     |   '(' expr ')'    #parens
     ;
 
+
 INT     : [0-9]+ ;
 FLOAT   : [0-9]+ '.' [0-9]+ ;
-BOOL    : 'bool';
-INTEGER : 'int';
-PRINT   : 'print';
-IF      : 'if';
 STRING  : '"' .*? '"' ;
-ID      : [a-zA-Z]+;
+ID      : [a-zA-Z_][a-zA-Z0-9_]* ;
 
 WS : [ \t\n\r]+ -> skip ;
--- ejemplosANTLR/small/cliente.py
+++ ejemplosANTLR/small/cliente.py
@@ -2,25 +2,149 @@ from antlr4 import *
 from antlr.SmallLexer import SmallLexer
 from antlr.SmallParser import SmallParser
 from antlr.SmallListener import SmallListener
-from walkers.declare import DeclareListener
-from walkers.typecheck import TypecheckListener
 import sys
 
 
-class TreePrinter(SmallListener):
-    pass
+# Listener para ver las declaraciones
+class DeclareListener(SmallListener):
+    def __init__(self):
+        self.variables = {}
+        self.functions = {}
+
+    # Variables
+    def enterVarDecl(self, ctx: SmallParser.VarDeclContext):
+        var_type = ctx.type_().getText()
+        for ident in ctx.ident_list().ident():
+            var_name = ident.ID().getText()
+            self.getVariables()[var_name] = var_type
+
+    def getVariables(self):
+        return self.variables
+
+    # Funciones
+    def enterFunction_decl(self, ctx: SmallParser.Function_declContext):
+        func_name = ctx.ID().getText()
+        return_type = ctx.type_().getText()
+        params = [(param.type_().getText(), param.ID().getText()) for param in ctx.params().param()] if ctx.params() \
+            else []
+        self.functions[func_name] = (return_type, params)
+
+    def getFunctions(self):
+        return self.functions
+
+
+# Listener para checar el tipo de dato
+class TypecheckListener(SmallListener):
+    def __init__(self, variables, functions):
+        self.variables = variables
+        self.functions = functions
+        self.errors = []
+        self.types = {}
+
+    # Declaración de variables
+    def enterAssign(self, ctx: SmallParser.AssignContext):
+        var_name = ctx.expr(0).getText()
+        if var_name not in self.variables:
+            self.errors.append(f"Error: Variable '{var_name}' no declarada.")
+
+    def enterVarInitStmt(self, ctx: SmallParser.VarInitStmtContext):
+        var_name = ctx.ID().getText()
+        var_type = ctx.type().getText()
+        self.funTipo = ctx.type_().getText()
+        if var_type != self.funTipo:
+            self.errors.append(f"Error: Asignación de tipo incorrecto a la variable '{var_name}")
+
+    # Declaración de funciones
+    def enterFunctionCall(self, ctx: SmallParser.FunctionCallContext):
+        func_name = ctx.ID().getText()
+        if func_name not in self.functions:
+            self.errors.append(f"Error: Función '{func_name}' no declarada.")
+
+    # Tipos de dato
+    def exitInt(self, ctx: SmallParser.IntContext):
+        self.types[ctx] = 'int'
+
+    def exitFloat(self, ctx: SmallParser.FloatContext):
+        self.types[ctx] = 'float'
+
+    def exitString(self, ctx: SmallParser.StringContext):
+        self.types[ctx] = 'string'
+
+    def exitTrue(self, ctx: SmallParser.TrueContext):
+        self.types[ctx] = 'boolean'
+
+    def exitFalse(self, ctx: SmallParser.FalseContext):
+        self.types[ctx] = 'boolean'
+
+    # Tipos de dato en return
+    def exitReturn(self, ctx: SmallParser.ReturnContext):
+        tipo = self.types[ctx.expr()]
+        if tipo != self.funTipo:
+            raise Exception("Tipos incompatibles")
+
+    # Tipos de dato en funciones
+    def enterFunction_decl(self, ctx: SmallParser.Function_declContext):
+        self.funTipo = ctx.type_().getText()
+
+    # Tipos de dato en operaciones básicas
+    def exitAdd(self, ctx: SmallParser.AddContext):
+        left = self.types[ctx.expr(0)]
+        right = self.types[ctx.expr(1)]
+
+        if left == right:
+            self.types[ctx] = left
+        else:
+            raise Exception("Tipos incompatibles")
+
+    def exitSub(self, ctx: SmallParser.AddContext):
+        left = self.types[ctx.expr(0)]
+        right = self.types[ctx.expr(1)]
+
+        if left == right:
+            self.types[ctx] = left
+        else:
+            raise Exception("Tipos incompatibles")
+
+    def exitMult(self, ctx: SmallParser.AddContext):
+        left = self.types[ctx.expr(0)]
+        right = self.types[ctx.expr(1)]
+
+        if left == right:
+            self.types[ctx] = left
+        else:
+            raise Exception("Tipos incompatibles")
+
+    def exitDiv(self, ctx: SmallParser.AddContext):
+        left = self.types[ctx.expr(0)]
+        right = self.types[ctx.expr(1)]
+
+        if left == right:
+            self.types[ctx] = left
+        else:
+            raise Exception("Tipos incompatibles")
+
+    def getErrors(self):
+        return self.errors
+
 
 
 def main(argv):
-    parser = SmallParser(CommonTokenStream(SmallLexer(FileStream("prueba.txt"))))
+    parser = SmallParser(CommonTokenStream(SmallLexer(FileStream("ejemplo3.txt"))))
     tree = parser.program()
-    declarations = DeclareListener()
 
+    declarations = DeclareListener()
     walker = ParseTreeWalker()
     walker.walk(declarations, tree)
 
-    #typecheck = TypecheckListener(declarations.getTypes())
-    #walker.walk(typecheck, tree)
+    typecheck = TypecheckListener(declarations.getVariables(), declarations.getFunctions())
+    walker.walk(typecheck, tree)
+
+    errors = typecheck.getErrors()
+    if errors:
+        for error in errors:
+            print(error)
+    else:
+        print("No se encontraron errores semánticos.")
 
 
 if __name__ == '__main__':
